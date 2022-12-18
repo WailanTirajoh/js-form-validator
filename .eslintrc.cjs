51927a07feb471d715e8606797365fe86b764aa6
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2022-12-18 17:16:08 +0700

    fix: eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,6 @@
 /* eslint-disable no-undef */
 module.exports = {
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
-	],
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
 	root: true,
