8fa25fb941ccc25a579579190d704f7c31d372e3
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2022-12-18 17:22:31 +0700

    feat: add eslint plugin prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 module.exports = {
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
 	root: true,
